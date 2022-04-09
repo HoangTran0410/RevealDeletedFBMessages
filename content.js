370f026644a3d30d7c6c1ad197378cb219780557
Hoang <th01634044850@gmail.com>
2022-04-09 20:04:33 +0700

    [HotFix] manifest 2 -> manifest 3

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 // ============================= inject websocket code ============================
 var inject_ws_script = document.createElement("script");
-inject_ws_script.src = chrome.extension.getURL("./inject_websocket.js");
+inject_ws_script.src = chrome.runtime.getURL("./inject_websocket.js");
 
 // Sau khi thêm vào DOM và chạy xong code thì tự xóa dấu vết
 inject_ws_script.onload = function () {
